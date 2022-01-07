fcf51932c185aae4e52b12ad7fef116706262d63
pradh <pradh@DESKTOP-765MFUR>
2022-01-07 01:45:22 +0100

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//Local Modules
+import img from './images/img-1.png';
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import useStyles from "./styles"
+
+
+const App = () =>{
+    const classes = useStyles();
+    return(
+        <Container maxWidth="lg" >
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className="classes.heading" variant="h2" align="center">Collection</Typography>
+                <img  calssName={classes.images} src={img}  alt="Image" width="60" height="60"  />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="strech" spacing={3}>
+                        <Grid item xs={12}sm={7}> 
+                                <Posts/>
+                        </Grid>
+                        <Grid item xs={12}sm={4}> 
+                                <Form/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
+    )
 }
-
 export default App;
